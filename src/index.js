ef0002be5bdbebc8d07536067d5115a0e2b1500c
vblong <vu09@ads.uni-passau.de>
2021-06-26 17:40:36 +0200

    Fix routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { HashRouter as Router } from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom"
 import TodoContainer from "./functionBased/components/TodoContainer"
 import "./App.css"
 
 ReactDOM.render(
     <React.StrictMode>
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
             <TodoContainer/>
         </Router>
     </React.StrictMode>, 
